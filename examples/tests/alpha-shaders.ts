ded635fd52f5eb09a51564f66d42a41b566cb348
Suresh Kumar Gangumalla <skgangumalla@invecas.local>
2024-06-04 17:43:58 +0530

    Added alpha-shaders test case
    Added new test case to demonstrate alpha and shaders (especially
    radius) dependency with transparent color of parent node
    
    Signed-off-by: Suresh Kumar Gangumalla <skgangumalla@invecas.local>

---
+++ examples/tests/alpha-shaders.ts
@@ -0,0 +1,137 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2023 Comcast Cable Communications Management, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import type { ExampleSettings } from '../common/ExampleSettings.js';
+
+export async function automation(settings: ExampleSettings) {
+  // Snapshot single page
+  await test(settings);
+  await settings.snapshot();
+}
+
+export default async function test({ renderer, testRoot }: ExampleSettings) {
+  /**
+   * For row elements, provided explicit color as 0x00000000, transparent color,
+   * if not provided observed node rendered with default white (0xffffffff)
+   */
+
+  /**
+   *  Observations: if we decrease the height of the child-element then alpha effect is
+   *  working as expected on row element.
+   */
+
+  /**
+   * Usage: Use Up/Down arrows to switch the alpha between two rows
+   *        Use 'a' key to change the row1 child element height to 200
+   *        Use 'b' key to change the row1 child element height back to original 400
+   * */
+
+  const colorOfRow = 0x00000000;
+
+  const heightOfChildElement = 400;
+
+  const row1 = renderer.createNode({
+    width: 1000,
+    height: 500,
+    x: 960,
+    mountX: 0.5,
+    alpha: 0.2,
+    color: colorOfRow,
+    parent: testRoot,
+  });
+
+  const row1Child = renderer.createNode({
+    y: 250,
+    x: 100,
+    mountY: 0.5,
+    width: 200,
+    height: heightOfChildElement,
+    color: 0xff0000ff,
+    shader: renderer.createShader('DynamicShader', {
+      effects: [
+        {
+          type: 'radius',
+          props: {
+            radius: 20,
+          },
+        },
+      ],
+    }),
+    parent: row1,
+  });
+
+  const row2 = renderer.createNode({
+    width: 1000,
+    height: 500,
+    x: 960,
+    mountX: 0.5,
+    alpha: 0.2,
+    y: 600,
+    color: colorOfRow,
+    parent: testRoot,
+  });
+
+  const row2Child = renderer.createNode({
+    x: 100,
+    y: 250,
+    mountY: 0.5,
+    width: 200,
+    height: heightOfChildElement,
+    color: 0x00ff00ff,
+    shader: renderer.createShader('DynamicShader', {
+      effects: [
+        {
+          type: 'radius',
+          props: {
+            radius: 20,
+          },
+        },
+      ],
+    }),
+    parent: row2,
+  });
+
+  let currentFocusIndex = 0;
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowUp') {
+      if (currentFocusIndex == 1) {
+        row1.alpha = 1;
+        row2.alpha = 0.2;
+        currentFocusIndex ^= 1;
+      }
+    }
+    if (e.key === 'ArrowDown') {
+      if (currentFocusIndex == 0) {
+        row2.alpha = 1;
+        row1.alpha = 0.2;
+        currentFocusIndex ^= 1;
+      }
+    } else if (e.key == 'a') {
+      // To verify alpha effect is working as expected when row1 child-element height got decreased
+      row1Child.height = 200;
+    } else if (e.key == 'b') {
+      // Setting height back to original
+      row1Child.height = heightOfChildElement;
+    }
+  });
+
+  // set row1 alpha to 1
+  setTimeout(() => (row1.alpha = 1), 200);
+}
